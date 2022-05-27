dec2a8cbd2ee2b0cdc8746c48dfaa42a52e15a21
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 14:27:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from Process.main import call_py, bot
+from helpers.main import call_py, bot
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
