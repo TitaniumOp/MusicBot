eaaf98625fe5bc5027c1294cf05cf63db25a4478
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 22:13:08 +0530

    Update Restart.py

--- Titanium/Plugins/Restart.py
+++ Titanium/Plugins/Restart.py
@@ -23,6 +23,16 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from helpers.queues import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
 from helpers.admin_check import *
 from helpers.main import bot, call_py
-
+from config import OWNER_ID
 app = call_py
 LIVE_CHATS = []
+
+
+
+@bot.on_message(filters.command("restart"))
+async def restart(_, message):
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    await message.reply_text("🛠 <i>Restarting Music Player...</i>")
+    os.system(f"kill -9 {os.getpid()} && python3 app.py")
