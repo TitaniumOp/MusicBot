c41641131e601f3c99b60755d2db7ea66acfc0f8
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 22:15:34 +0530

    Update Playlist.py

--- Titanium/Plugins/Playlist.py
+++ Titanium/Plugins/Playlist.py
@@ -26,3 +26,29 @@ from helpers.main import bot, call_py
 
 app = call_py
 LIVE_CHATS = []
+
+
+
+@bot.on_message(filters.command(["playlist", "queue"]) & filters.group)
+@is_admin
+async def playlist(_, message):
+    chat_id = message.chat.id
+    if chat_id in QUEUE:
+        chat_queue = get_queue(chat_id)
+        if len(chat_queue) == 1:
+            await message.delete()
+            await message.reply_text(
+                f"▶️ <b>Now playing:</b> [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][4]}`",
+                disable_web_page_preview=True,
+            )
+        else:
+            out = f"<b>📃 Player queue:</b> \n\n▶️ <b>Now playing:</b> [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][4]}` \n"
+            l = len(chat_queue)
+            for x in range(1, l):
+                title = chat_queue[x][0]
+                link = chat_queue[x][2]
+                type = chat_queue[x][4]
+                out = out + "\n" + f"<b>#️⃣ {x}</b> - [{title}]({link}) | `{type}` \n"
+            await message.reply_text(out, disable_web_page_preview=True)
+    else:
+        await message.reply_text("❗Nothing is playing.")
