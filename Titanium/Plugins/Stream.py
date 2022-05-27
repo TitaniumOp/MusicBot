6832d1f4cc88d7b23a54363a6c874cc3bad731f0
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 22:03:22 +0530

    Create Stream.py

---
+++ Titanium/Plugins/Stream.py
@@ -0,0 +1,68 @@
+import os
+import glob
+import json
+import logging
+import asyncio
+import youtube_dl
+from pytgcalls import StreamType
+from pytube import YouTube
+from youtube_search import YoutubeSearch
+from pytgcalls import PyTgCalls, idle
+from pytgcalls.types import Update
+from pyrogram.raw.base import Update
+from pytgcalls.types import AudioPiped, AudioVideoPiped
+from pytgcalls.types import (
+    HighQualityAudio,
+    HighQualityVideo,
+    LowQualityVideo,
+    MediumQualityVideo
+)
+from pytgcalls.types.stream import StreamAudioEnded, StreamVideoEnded
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from helpers.queues import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
+from helpers.admin_check import *
+from helpers.main import bot, call_py
+
+app = call_py
+
+
+
+LIVE_CHATS = []
+
+@bot.on_message(filters.command(["astream", "vstream"]) & filters.group)
+@is_admin
+async def stream_func(_, message):
+    await message.delete()
+    state = message.command[0].lower()
+    try:
+        link = message.text.split(None, 1)[1]
+    except:
+        return await message.reply_text(f"<b>Usage:</b> <code>/{state} [link]</code>")
+    chat_id = message.chat.id
+    
+    if state == "saudio":
+        damn = AudioPiped
+        emj = "🎵"
+    elif state == "svideo":
+        damn = AudioVideoPiped
+        emj = "🎬"
+    m = await message.reply_text("🔄 Processing...")
+    try:
+        if chat_id in QUEUE:
+            return await m.edit("❗️Please send <code>/stop</code> to end voice chat before live streaming.")
+        elif chat_id in LIVE_CHATS:
+            await app.change_stream(
+                chat_id,
+                damn(link)
+            )
+            await m.edit(f"{emj} Started streaming: [Link]({link})", disable_web_page_preview=True)
+        else:    
+            await app.join_group_call(
+                chat_id,
+                damn(link),
+                stream_type=StreamType().pulse_stream)
+            await m.edit(f"{emj} Started streaming: [Link]({link})", disable_web_page_preview=True)
+            LIVE_CHATS.append(chat_id)
+    except Exception as e:
+        return await m.edit(str(e))
