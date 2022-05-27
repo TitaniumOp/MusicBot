0a6ac5b37747502c789367dc31337ad0dcd8e8c5
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 22:06:33 +0530

    Create Skip.py

---
+++ Titanium/Plugins/Skip.py
@@ -0,0 +1,56 @@
+import os
+import glob
+import json
+import logging
+import asyncio
+import youtube_dl
+from pytgcalls import StreamType
+from pytube import YouTube
+from youtube_search import YoutubeSearch
+from pytgcalls import PyTgCalls, idle
+from pytgcalls.types import Update
+from pyrogram.raw.base import Update
+from pytgcalls.types import AudioPiped, AudioVideoPiped
+from pytgcalls.types import (
+    HighQualityAudio,
+    HighQualityVideo,
+    LowQualityVideo,
+    MediumQualityVideo
+)
+from pytgcalls.types.stream import StreamAudioEnded, StreamVideoEnded
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from helpers.queues import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
+from helpers.admin_check import *
+from helpers.main import bot, call_py
+
+app = call_py
+LIVE_CHATS = []
+
+@bot.on_message(filters.command("skip") & filters.group)
+@is_admin
+async def skip(_, message):
+    await message.delete()
+    chat_id = message.chat.id
+    if len(message.command) < 2:
+        op = await skip_current_song(chat_id)
+        if op == 0:
+            await message.reply_text("❗️Nothing in the queue to skip.")
+        elif op == 1:
+            await message.reply_text("❗️Empty queue, stopped streaming.")
+    else:
+        skip = message.text.split(None, 1)[1]
+        out = "🗑 <b>Removed the following song(s) from the queue:</b> \n"
+        if chat_id in QUEUE:
+            items = [int(x) for x in skip.split(" ") if x.isdigit()]
+            items.sort(reverse=True)
+            for x in items:
+                if x == 0:
+                    pass
+                else:
+                    hm = await skip_item(chat_id, x)
+                    if hm == 0:
+                        pass
+                    else:
+                        out = out + "\n" + f"<b>#️⃣ {x}</b> - {hm}"
+            await message.reply_text(out)
