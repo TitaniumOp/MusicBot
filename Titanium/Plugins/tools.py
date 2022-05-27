128c52bb2fb7aa479a83a9da8015d81624d09553
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 22:14:13 +0530

    Update tools.py

--- Titanium/Plugins/tools.py
+++ Titanium/Plugins/tools.py
@@ -1,3 +1,33 @@
+import os
+import glob
+import json
+import logging
+import asyncio
+import youtube_dl
+from pytgcalls import StreamType
+from pytube import YouTube
+from youtube_search import YoutubeSearch
+from pytgcalls import PyTgCalls, idle
+from pytgcalls.types import Update
+from pyrogram.raw.base import Update
+from pytgcalls.types import AudioPiped, AudioVideoPiped
+from pytgcalls.types import (
+    HighQualityAudio,
+    HighQualityVideo,
+    LowQualityVideo,
+    MediumQualityVideo
+)
+from pytgcalls.types.stream import StreamAudioEnded, StreamVideoEnded
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from helpers.queues import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
+from helpers.admin_check import *
+from helpers.main import bot, call_py
+from config import OWNER_ID
+app = call_py
+LIVE_CHATS = []
+
+
 
 @bot.on_message(filters.command("stop") & filters.group)
 @is_admin
