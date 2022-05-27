7bd3c39baee7d6ecb747444684cbb29560abd547
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-05-27 14:21:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 import asyncio
 from pytgcalls import idle
-#from config import api_id, api_hash, bot_token, session_name
-import os
-import sys
-import random
-import telethon.utils
-from telethon import TelegramClient, events
-from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
-from pytgcalls import PyTgCalls
-from pyrogram import Client
+from Process.main import call_py, bot
 
-bot = Client(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins={"root": "handlers"},
-)
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    print("[INFO]: STARTING PYTGCALLS CLIENT")
+    await call_py.start()
+    await idle()
+    print("[INFO]: STOPPING BOT & USERBOT")
+    await bot.stop()
 
-BOT = TelegramClient('BOT', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-user = Client(
-    SESSION_NAME,
-    api_id=API_ID,
-    api_hash=API_HASH,
-)
-op = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'handlers'})
-app = PyTgCalls(op)
-
-bot.run
-app.start
-
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
