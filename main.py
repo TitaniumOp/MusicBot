eaef2dda7a8fa50b58c929fd2d711e9b27dde56e
TITANIUM XYZ <92505709+TITANIUMXDD@users.noreply.github.com>
2022-06-18 13:20:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from pytgcalls import idle
 from helpers.main import call_py, bot
 
 async def start_bot():
-    print("[INFO]: STARTING BOT CLIENT")
-    await bot.start()
+    #print("[INFO]: STARTING BOT CLIENT")
+    #await bot.start()
     print("[INFO]: STARTING PYTGCALLS CLIENT")
     await call_py.start()
     await idle()
-    print("[INFO]: STOPPING BOT & USERBOT")
-    await bot.stop()
+    #print("[INFO]: STOPPING BOT & USERBOT")
+    #await bot.stop()
 
 
 loop = asyncio.get_event_loop()
